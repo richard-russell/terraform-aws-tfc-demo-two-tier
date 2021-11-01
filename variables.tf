dea6598f3abbdabeebc8adc26fa17ebc72bf2cb2
Richard Russell <rirussel@cisco.com>
2021-11-01 16:52:53 +0000

    Remove region input variable and AMI default

--- variables.tf
+++ variables.tf
@@ -8,19 +8,9 @@
 # DESCRIPTION
 # }
 
-# variable "key_name" {
-#   description = "Desired name of AWS key pair"
-# }
-
-variable "aws_region" {
-  description = "AWS region to launch servers."
-  default     = "eu-west-2"
-}
-
 # Ubuntu Precise 12.04 LTS (x64)
 variable "aws_ami" {
   description = "The AWS AMI to use for the web server"
-  default = "ami-02c7ed3df628d7ba2"
 }
 
 variable "service_name" {
